584d6e50fb0efd843a8483c3b048ec5419b7fea9
HARSHVARDHAN PUROHIT <75427196+illumintor1609@users.noreply.github.com>
2021-06-08 18:14:41 +0530

    This is my initial commit
    You can download and play this game moreover you can modify it but don't forget to push it back in the updated version like never before.

---
+++ jumanji.py
@@ -0,0 +1,182 @@
+print(''''           
+                                                                                                                                                                 
+          JJJJJJJJJJJUUUUUUUU     UUUUUUUUMMMMMMMM               MMMMMMMM               AAA               NNNNNNNN        NNNNNNNN          JJJJJJJJJJJIIIIIIIIII
+          J:::::::::JU::::::U     U::::::UM:::::::M             M:::::::M              A:::A              N:::::::N       N::::::N          J:::::::::JI::::::::I
+          J:::::::::JU::::::U     U::::::UM::::::::M           M::::::::M             A:::::A             N::::::::N      N::::::N          J:::::::::JI::::::::I
+          JJ:::::::JJUU:::::U     U:::::UUM:::::::::M         M:::::::::M            A:::::::A            N:::::::::N     N::::::N          JJ:::::::JJII::::::II
+            J:::::J   U:::::U     U:::::U M::::::::::M       M::::::::::M           A:::::::::A           N::::::::::N    N::::::N            J:::::J    I::::I  
+            J:::::J   U:::::D     D:::::U M:::::::::::M     M:::::::::::M          A:::::A:::::A          N:::::::::::N   N::::::N            J:::::J    I::::I  
+            J:::::J   U:::::D     D:::::U M:::::::M::::M   M::::M:::::::M         A:::::A A:::::A         N:::::::N::::N  N::::::N            J:::::J    I::::I  
+            J:::::j   U:::::D     D:::::U M::::::M M::::M M::::M M::::::M        A:::::A   A:::::A        N::::::N N::::N N::::::N            J:::::j    I::::I  
+            J:::::J   U:::::D     D:::::U M::::::M  M::::M::::M  M::::::M       A:::::A     A:::::A       N::::::N  N::::N:::::::N            J:::::J    I::::I  
+JJJJJJJ     J:::::J   U:::::D     D:::::U M::::::M   M:::::::M   M::::::M      A:::::AAAAAAAAA:::::A      N::::::N   N:::::::::::NJJJJJJJ     J:::::J    I::::I  
+J:::::J     J:::::J   U:::::D     D:::::U M::::::M    M:::::M    M::::::M     A:::::::::::::::::::::A     N::::::N    N::::::::::NJ:::::J     J:::::J    I::::I  
+J::::::J   J::::::J   U::::::U   U::::::U M::::::M     MMMMM     M::::::M    A:::::AAAAAAAAAAAAA:::::A    N::::::N     N:::::::::NJ::::::J   J::::::J    I::::I  
+J:::::::JJJ:::::::J   U:::::::UUU:::::::U M::::::M               M::::::M   A:::::A             A:::::A   N::::::N      N::::::::NJ:::::::JJJ:::::::J  II::::::II
+ JJ:::::::::::::JJ     UU:::::::::::::UU  M::::::M               M::::::M  A:::::A               A:::::A  N::::::N       N:::::::N JJ:::::::::::::JJ   I::::::::I
+   JJ:::::::::JJ         UU:::::::::UU    M::::::M               M::::::M A:::::A                 A:::::A N::::::N        N::::::N   JJ:::::::::JJ     I::::::::I
+     JJJJJJJJJ             UUUUUUUUU      MMMMMMMM               MMMMMMMMAAAAAAA                   AAAAAAANNNNNNNN         NNNNNNN     JJJJJJJJJ       IIIIIIIIII
+                                                                                                                                                                 
+                                                                                                                                                                 
+                                                                                                                                                                 ''')
+print("Welcome to JUMANJI \n")
+print("Your mission is to put the stone on the mountain\n")
+name = input(("Please enter your name : "))
+avatar = input(
+    f"Great going {name}, \n Please Select and Write the name of the Charcter from : \n DR BRAVESTONE \n SHELLY OSBON \n RUBY ROUNDHOUSE \n MOOUSE FINBAR : ")
+print(f"Hello {avatar} , Welome to jumanji I am your personal guide so we have called you to make our jumanji threat free \n By putting the gem again into the jaguar on the mountain will make us free from all the efforts \n")
+print(''' 
+                                     _,.--""--.,,_              .' .-'
+             ,       ,   _,,_ _,.--''             `""-._       / .'
+             \`'-""-' |-"    `           ,    _.'-.,_   `-, _.' /
+              | e  e        ,    .      /    ';-..._ `'-,_ ` _.'
+           _..\      /       )    \ _,.-'.     \    `)  | ```
+  __,.--"'`   _\_Y_ /.___,.-'   _.'`      `>   /    /  /
+(((_.--'--""``  '--'__.-'  __.-'      .---'   / .--' .'
+           jgs    (((_..--'          ((_(_.-'` ((_(.' ''')
+print("BUT DON'T FORGET BOAT IS JUST LIKE GAME SO SIMPLE BUT FAST BEHAVIOUR OF BOAT IS ALSO REQUIRED \n")
+decision_1 = input(
+    "You are in the boat write YES to continue with boat and NO to walk on the path: \n").lower()
+if decision_1 == 'yes':
+    print("You are going Great in the game and passed level 1 \n")
+    decision_2_decision_1 = input(
+        "Want to drive boat fast or slow something is going bad \n Write FAST to drive fast and MODERATE to drive moderate : ").lower()
+    if decision_2_decision_1 == "fast":
+        print("YHA YOU CHOOSED CORRECT BECASUE THAT EVIL CROCODILE IS LOOKING FOR YOU")
+        print(''' 
+
+                     _.---._     .---.
+            __...---' .---. `---'-.   `.
+  ~ -~ -.-''__.--' _.'( | )`.  `.  `._ :
+ -.~~ .'__-'_ .--'' ._`---'_.-.  `.   `-`.
+  ~ ~_~-~-~_ ~ -._ -._``---. -.    `-._   `.
+    ~- ~ ~ -_ -~ ~ -.._ _ _ _ ..-_ `.  `-._``--.._
+     ~~-~ ~-_ _~ ~-~ ~ -~ _~~_-~ -._  `-.  -. `-._``--.._.--''. ~ -~_
+         ~~ -~_-~ _~- _~~ _~-_~ ~-_~~ ~-.___    -._  `-.__   `. `. ~ -_~
+       jgs   ~~ _~- ~~- -_~  ~- ~ - _~~- _~~ ~---...__ _    ._ .` `. ~-_~
+                ~ ~- _~~- _-_~ ~-_ ~-~ ~_-~ _~- ~_~-_~  ~--.....--~ -~_ ~
+                     ~ ~ - ~  ~ ~~ - ~~-  ~~- ~-  ~ -~ ~ ~ -~~-  ~- ~-~
+
+ ''')
+        decision3_decision2_decision3 = input(
+            "Yoy have reached to jungle ! \n Now choose LEFT OR RIGHT DIRECTON TO WALK IN THE JUNGLE \n").lower()
+        if(decision3_decision2_decision3 == 'left'):
+            print('''___¶¶¶¶_______________¶¶¶¶¶_______
+_______¶___¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶___¶_______
+______¶¶¶___¶_____¶_____¶____¶¶_______
+______¶¶¶¶¶___¶¶¶¶¶_¶¶_¶¶¶¶__¶________
+_____¶¶__¶_¶¶__¶_¶¶¶¶_¶_¶¶_¶¶¶________
+____¶__¶¶¶_¶¶¶__¶_____¶¶¶_¶¶¶¶¶_______
+____¶_¶_¶¶_¶¶¶¶_____¶¶¶¶¶_¶¶_¶_¶______
+___¶_¶¶_¶¶___¶¶_____¶___¶__¶_¶_¶¶_____
+___¶_¶__¶¶_¶_¶_______¶_¶¶¶¶_¶__¶¶_____
+___¶_¶____¶¶¶¶¶¶¶¶¶¶¶¶¶¶__¶_¶___¶_____
+___¶¶¶___¶____¶¶_¶¶¶___¶¶_¶_¶___¶¶____
+___¶¶¶___¶¶¶____¶¶_____¶¶_¶¶___¶¶_____
+___¶_¶¶¶___¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶____¶¶______
+___¶__¶¶¶¶¶__¶_¶____¶_¶¶¶___¶¶¶_¶_____
+___¶_____¶_¶_¶¶¶____¶_¶___¶_¶__¶¶_____
+___¶___¶___¶¶_¶¶___¶¶_¶_¶_¶¶___¶¶¶____
+__¶_____¶___¶¶¶_¶¶¶¶¶_¶¶_¶¶____¶_¶____
+__¶_____¶____¶¶__¶¶__¶___¶¶___¶¶_¶____
+__¶_____¶¶____¶¶___¶¶¶¶¶¶¶____¶¶_¶____
+__¶______¶_____¶¶¶¶___¶¶_¶____¶__¶____
+__¶__¶____¶____¶¶¶____¶_¶_____¶__¶____
+__¶_¶_¶___¶¶____¶__¶__¶¶_____¶___¶____
+_¶___¶_____¶¶____¶¶_¶_______¶____¶____
+_¶____¶_____¶¶_____¶¶_____¶¶_____¶____
+_¶____¶______¶¶__________¶¶_____¶_¶___
+_¶_____¶______¶_________¶¶____¶¶__¶___
+_¶______¶¶____¶_____________¶¶¶___¶___
+_¶________¶_____________¶¶¶¶¶_____¶___
+_¶________¶¶___________¶__________¶___
+_¶________¶¶_________¶¶¶_____¶¶___¶___
+_¶¶____¶___¶¶¶¶¶¶¶¶¶¶¶_¶___¶¶_____¶___
+¶_¶¶¶¶_¶¶¶¶¶__________¶¶¶¶______¶¶____
+¶_¶__¶¶¶__¶¶___________¶¶¶¶_____¶¶____
+_¶¶_______¶¶____________¶_______¶_____
+__¶¶______¶_____________¶¶______¶_____
+___¶______¶______________¶______¶_____
+___¶______¶_______________¶_____¶_____
+___¶______¶_______________¶______¶____
+___¶______¶¶______________¶______¶¶¶__
+__¶________¶______________¶________¶¶_
+_¶¶_________¶_____________¶_________¶¶
+¶¶_________¶¶_____________¶_____¶___¶_
+¶¶_¶_______¶______________¶¶¶¶___¶¶¶__
+__¶¶__¶___¶¶__________________¶¶¶¶____
+____¶¶¶¶¶¶¶¶__________________________ ''')
+
+            print("TIGER EATEN YOU! GAME OVER.")
+
+        elif (decision3_decision2_decision3 == 'right'):
+            decision3_decision2_decision3_decision4 = input("NOW YOU HAVE REACHED TO THE LAST LEVEL SO HERE IS THE LARGE MOUNTIAN \n NOW TAKE RIDE ON MOUNTAIN BIKE \n BY RIDING BIKE \n AND YOU CAN ALSO CHOOSE THE CLIMBING OPTION BY WRITING \n CLIMB IN THE TEXT").lower()
+            if decision3_decision2_decision3_decision4 == 'bike':
+                decision3_decision2_decision3_decision4_decision5 = input(
+                    "PLEASE ENTER \n FAST \n TO SPEED UP THE BIKE ON THE MOUNTAIN \n OR WRITE SLOW TO DRIVE THE VECHICAL WITH SLOW SPEED \n ").lower()
+                if decision3_decision2_decision3_decision4_decision5 == 'fast':
+                    print(
+                        f"YOU FALL FROM THE MOUNTAIN \n GAME OVERR ! \n SUCH A NICE Performance by {name}")
+                elif decision3_decision2_decision3_decision4_decision5 == 'slow':
+                    print(f"YOU FINALLY REACHED THE PLACE ! CONGRATS {avatar}")
+                    final = input("Please enter JUMANJI: ").lower()
+                    if final == 'jumaji':
+                      print("YOU WONT THE GAME ! \n")
+                else:
+                    print("GAME OVER!")
+            elif decision3_decision2_decision3_decision4 == 'climb':
+                print("Some monkey came their and killed you \n GAME OVER!")
+
+    elif decision_2_decision_1 == 'slow':
+        print(''' 
+
+       ,       ,
+      /|    |\./'.
+     | |  ,  \|| ,|
+     \  \_(\.-""\//.  _
+   .-'`""``"` _   ` `-.`"""--.._      _..----. __
+   | '~`      o\                `"---"        `. `"-.==,
+    \,.-;    `"`                                |`""`===`
+      (`            /                           |
+       `-----.____.;          \     |           ;
+                   \__         |    \          /
+                  .'         .'      \        ' `,
+                 /          /         '._        |
+                 |    '.---;`-.____.-'`\ `""`;   |
+                 |     _\   \    '.     )   /    \
+                 \-,--( /   /    _/   .'   |_ _ .-)
+              jgs '----;)__;    (`.-. ;    `-:.;-'
+                                 `""""`''')
+        print("RHINO KILLED YOU GAME OVER!")
+
+elif decision_1 == 'no':
+    print(f"Now You are on the skit road where all the dangers are awaiting \n")
+    decision_1_decision9 = input(
+        f"Write left if you want to go to left \n and right to go on the right \n").lower()
+    if decision_1_decision9 == 'left':
+        print("Tiger killed You ! GAME OVER!")
+    elif decision_1_decision9 == 'right':
+        decision_1_decision10 = input(
+            "Now you are in the house! want to take rest \n then write rest \nbut if want to continue write continue\n").lower()
+        if decision_1_decision10 == 'rest':
+            print('Tiger killed you in the Night ! GAME OVER !')
+        elif decision_1_decision10 == 'continue':
+            print(
+                f' Now there are two ways to reach the mountains \n {avatar} you have to make a choice \n')
+            decision_1_decision_13 = input(
+                "Now first choice is to go by  bike \n and second choice is to go by car \n Write bike for selecting bike \n Write car for selecting car\n").lower()
+            if decision_1_decision_13 == 'bike':
+                print("Now you have reached to the mountain \n")
+                decision_1_decision19 = input(
+                    "Now write climb to climb the mountain \n And write track for tracking on the mountian \n").lower()
+                if decision_1_decision19 == 'climb':
+                    print('You have been killed by the wild monkeys \n GAME OVER! \n')
+                elif decision_1_decision19 == 'track':
+                    print('You have been reached to the jaguar to apply the gem \n')
+                    decision_23 = input(
+                        "Wwrite Jumanji to end the game \n").lower()
+                    if decision_23 == 'jumanji':
+                        print('You won the game \n')
+            elif decision_1_decision_13 == 'car':
+              print(f"{avatar} you are killed by poisnous spider in the car \n GAME OVER !")
+        
